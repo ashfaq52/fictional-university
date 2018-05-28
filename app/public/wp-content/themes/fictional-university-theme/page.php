5bf3c32a402ad822f5d164515d32f77d01501c0d
Julia Gao Miller <julia.gao.miller@gmail.com>
2018-05-28 12:25:06 -0400

    Finished Section 16 - Menu of Child Page Links

--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -19,14 +19,15 @@
   <div class="container container--narrow page-section">
 
     <?php
-      $theParent = wp_get_post_parent_id(get_the_id());
-      if ($theParent) { ?>
-        echo "I am a child page.";
+      //gets the ID of the parent post; if there is an ID, means the page is a child page.
+      $parentPageID = wp_get_post_parent_id(get_the_id());
+      if ($parentPageID) { ?>
+        <!-- echo "I am a child page."; -->
         <div class="metabox metabox--position-up metabox--with-home-link">
           <p>
-            <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>">
+            <a class="metabox__blog-home-link" href="<?php echo get_permalink($parentPageID); ?>">
               <i class="fa fa-home" aria-hidden="true"></i>
-              Back to <?php echo get_the_title($theParent); ?>
+              Back to <?php echo get_the_title($parentPageID); ?>
             </a>
           <span class="metabox__main"><?php the_title(); ?></span>
           </p>
@@ -35,16 +36,33 @@
     ?>
 
 
-
-
-
-    <!-- <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
-      <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
-      </ul>
-    </div> -->
+    <?php
+      // only fills up the array if page is a child
+      $testArray = get_pages(array(
+        'child_of' => get_the_ID()
+      ));
+      // Only display menu if the page has a parent or the page is a parent
+      if($parentPageID or $testArray) { ?>
+      <div class="page-links">
+        <h2 class="page-links__title"><a href="<?php echo get_permalink($parentPageID); ?>"><?php echo get_the_title($parentPageID); ?></a></h2>
+        <ul class="min-list">
+          <?php
+            if($parentPageID){
+              //returns the ID of the parent page.
+              $findChildrenOf = $parentPageID;
+            } else {
+              //returns the ID of the current page
+              $findChildrenOf = get_the_ID();
+            }
+            wp_list_pages(array(
+              'title_li' => NULL,
+              'child_of' => $findChildrenOf,
+              'sort_column' => 'menu_order',
+            ));
+          ?>
+        </ul>
+      </div>
+    <?php } ?>
 
     <div class="generic-content">
       <?php the_content(); ?>
