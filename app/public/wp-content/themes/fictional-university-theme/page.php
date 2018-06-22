51ccca52507bb07ed9f8b96cf343540a9e85a670
Julia Gao Miller <julia.gao.miller@gmail.com>
2018-06-22 11:03:00 -0400

    Lecture 42: Reduce duplicate code

--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -3,17 +3,16 @@
   get_header();
 
   while(have_posts()) {
-    the_post(); ?>
+    the_post();
+    pageBanner(array(
+      'title' => 'Hello',
+      'subtitle' => 'this is the subtitle',
+      'photo' => "https://www.bestvalueschools.com/wp-content/uploads/2015/06/UC-San-Diego1-1024x786.jpg"
 
-  <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg')?>);"></div>
-    <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title"><?php the_title(); ?></h1>
-      <div class="page-banner__intro">
-        <p>DON'T FORGET TO REPLACE ME LATER</p>
-      </div>
-    </div>
-  </div>
+    ));
+    ?>
+
+
 
 
   <div class="container container--narrow page-section">
