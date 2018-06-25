8891ed65f54a62b2433b9314cb4826e22c0f3638
Julia Gao Miller <julia.gao.miller@gmail.com>
2018-06-24 20:34:37 -0400

    Lecture 47: Campus Posts Continued

--- app/sql/local.wp_usermeta.sql
+++ app/sql/local.wp_usermeta.sql
@@ -17,8 +17,12 @@ INSERT INTO `wp_usermeta` VALUES
 (13,1,"wp_user_level","10"),
 (14,1,"dismissed_wp_pointers","wp496_privacy"),
 (15,1,"show_welcome_panel","1"),
-(16,1,"session_tokens","a:1:{s:64:\"f30a564c7fcea6e20ad63b8d1b7e12991733c6ebf35d278fb14f3800e65a6280\";a:4:{s:10:\"expiration\";i:1529689214;s:2:\"ip\";s:9:\"127.0.0.1\";s:2:\"ua\";s:121:\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36\";s:5:\"login\";i:1529516414;}}"),
-(17,1,"wp_dashboard_quick_press_last_post_id","47"),
+(16,1,"session_tokens","a:2:{s:64:\"23aba31be81618f04c4188e1f0f931a3504944f4db23c1fff47f5f97829e15b4\";a:4:{s:10:\"expiration\";i:1529861496;s:2:\"ip\";s:9:\"127.0.0.1\";s:2:\"ua\";s:119:\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1.1 Safari/605.1.15\";s:5:\"login\";i:1529688696;}s:64:\"979834c4e31705f072b3c3d5157d95b1d431a11252f3dfcdbfe502c4b101774c\";a:4:{s:10:\"expiration\";i:1529876974;s:2:\"ip\";s:9:\"127.0.0.1\";s:2:\"ua\";s:121:\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36\";s:5:\"login\";i:1529704174;}}"),
+(17,1,"wp_dashboard_quick_press_last_post_id","75"),
 (18,1,"community-events-location","a:1:{s:2:\"ip\";s:12:\"192.168.95.0\";}"),
 (19,1,"managenav-menuscolumnshidden","a:5:{i:0;s:11:\"link-target\";i:1;s:11:\"css-classes\";i:2;s:3:\"xfn\";i:3;s:11:\"description\";i:4;s:15:\"title-attribute\";}"),
-(20,1,"metaboxhidden_nav-menus","a:1:{i:0;s:12:\"add-post_tag\";}");
+(20,1,"metaboxhidden_nav-menus","a:1:{i:0;s:12:\"add-post_tag\";}"),
+(21,1,"closedpostboxes_post","a:0:{}"),
+(22,1,"metaboxhidden_post","a:7:{i:0;s:12:\"revisionsdiv\";i:1;s:13:\"trackbacksdiv\";i:2;s:10:\"postcustom\";i:3;s:16:\"commentstatusdiv\";i:4;s:11:\"commentsdiv\";i:5;s:7:\"slugdiv\";i:6;s:9:\"authordiv\";}"),
+(23,1,"wp_user-settings","libraryContent=browse"),
+(24,1,"wp_user-settings-time","1529672486");
