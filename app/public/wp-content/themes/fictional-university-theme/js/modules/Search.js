8cd14d9f9f29ae92efb44605a21c5411e7362466
Julia Gao Miller <julia.gao.miller@gmail.com>
2018-06-27 22:38:19 -0400

    Lecture 51: Search bar continued. S and ESC key.

--- app/public/wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ app/public/wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -6,6 +6,7 @@ class Search {
         this.openButton = $(".js-search-trigger");
         this.closeButton = $(".search-overlay__close");
         this.searchOverlay = $(".search-overlay");
+        this.isOverlayOpen = false;
         this.events();
     }
 
@@ -13,15 +14,31 @@ class Search {
     events(){
         this.openButton.on('click', this.openOverlay.bind(this));
         this.closeButton.on('click', this.closeOverlay.bind(this));
+        $(document).on("keydown",this.keyPressDispatcher.bind(this));
+
     }
 
     //3. methods
+    keyPressDispatcher(e){
+
+        if(e.keyCode == 83 && !this.isOverlayOpen){
+            this.openOverlay();
+        } else if(e.keyCode == 27 && this.isOverlayOpen){
+            this.closeOverlay();
+        }
+    }
+
     openOverlay(){
         this.searchOverlay.addClass("search-overlay--active");
+        this.isOverlayOpen = true
+        $("body").addClass("body-no-scroll");
+
     }
 
     closeOverlay(){
         this.searchOverlay.removeClass("search-overlay--active");
+        this.isOverlayOpen = false;
+        $("body").removeClass("body-no-scroll");
     }
 }
 
