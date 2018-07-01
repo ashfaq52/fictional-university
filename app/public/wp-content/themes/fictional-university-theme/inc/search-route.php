6ac9cdbc163e8d286af66a52e268589a36abaca2
Julia Gao Miller <julia.gao.miller@gmail.com>
2018-07-01 17:05:56 -0400

    Lecture 66: Custom Layout & SON based on Post type

--- app/public/wp-content/themes/fictional-university-theme/inc/search-route.php
+++ app/public/wp-content/themes/fictional-university-theme/inc/search-route.php
@@ -11,7 +11,7 @@ function universityRegisterSearch(){
 
 function universitySearchResults($data){
     $mainQuery = new WP_Query(array(
-        'post_type' => array('post', 'page','professor'),
+        'post_type' => array('post', 'page','professor', 'campus', 'event'),
         //sanitize_text_field is for security reasons
         's' => sanitize_text_field($data['term']),
     ));
@@ -38,6 +38,7 @@ function universitySearchResults($data){
             array_push($results['professors'], array(
                 'title' => get_the_title(),
                 'permalink' => get_the_permalink(),
+                'image' => get_the_post_thumbnail_url(0, 'professorLandscape'),
             ));
         }
         if (get_post_type() == 'program'){
@@ -47,15 +48,26 @@ function universitySearchResults($data){
             ));
         }
         if (get_post_type() == 'event'){
+            $eventDate = new DateTime(get_field('event_date'));
+            $description = null;
+            if(has_excerpt()){
+                $description = get_the_excerpt();
+            } else {
+                $description = wp_trim_words(get_the_content(), 18);
+            }
             array_push($results['events'], array(
                 'title' => get_the_title(),
                 'permalink' => get_the_permalink(),
+                'month' => $eventDate->format('M'),
+                'day' => $eventDate->format('d'),
+                'description' => $description,
             ));
         }
         if (get_post_type() == 'campus'){
             array_push($results['campuses'], array(
                 'title' => get_the_title(),
                 'permalink' => get_the_permalink(),
+
             ));
         }
 
