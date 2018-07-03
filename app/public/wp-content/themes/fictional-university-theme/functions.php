610444f0cc934c5393c04c81f70452bd7ce0d493
Julia Gao Miller <julia.gao.miller@gmail.com>
2018-07-03 07:01:03 -0400

    Lecture 74 – Open Registration Part 4
    Login Screen Branding...

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 require get_theme_file_path('/inc/search-route.php');
 
 function university_custom_rest(){
@@ -124,5 +126,26 @@ function noSubsAdminBar(){
   if (count($ourCurrentUser->roles) == 1 AND $ourCurrentUser->roles[0] == 'subscriber') {
     show_admin_bar(false);
   }
+}
+
+//Customize Login Screen
+add_filter('login_headerurl', 'ourHeaderUrl');
+
+function ourHeaderUrl() {
+  return  esc_url(site_url('/'));
+}
+
+add_action('login_enqueue_scripts', 'ourLoginCSS');
+
+function ourLoginCSS() {
+  wp_enqueue_style('custom_google_fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+  wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+
+}
+
+add_filter('login_headertitle', 'ourLoginTitle');
+
+function ourLoginTitle() {
+  return get_bloginfo('name');
 }
 
